21b84cfb2c60401478e815ed290d3412c5a9997c
Rodolfo Rodrigues <rudwolf@gmail.com>
2022-11-04 17:09:54 -0300

    Added new expense form

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './ExpenseForm.scss';
+
+const ExpenseForm = () => {
+  return <form>
+    <div className="new-expense__controls">
+      <div className="new-expense__control">
+        <label htmlFor="">Title</label>
+        <input type="text" name="" id="" />
+      </div>
+      <div className="new-expense__control">
+        <label htmlFor="">Amount</label>
+        <input type="number" min="0.01" step="0.01" name="" id="" />
+      </div>
+      <div className="new-expense__control">
+        <label htmlFor="">Date</label>
+        <input type="date" min="2019-01-01" max="2022-12-31" name="" id="" />
+      </div>
+    </div>
+    <div className="new-expense__actions">
+      <button type="submit">Add Expense</button>
+    </div>
+  </form>;
+}
+
+export default ExpenseForm;
---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './NewExpense.scss';
+import ExpenseForm from './ExpenseForm';
+
+const NewExpense = () => {
+  return (
+    <div className='new-expense'>
+      <ExpenseForm />
+    </div>
+  );
+}
+
+export default NewExpense;
