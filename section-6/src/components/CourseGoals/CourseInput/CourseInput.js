ffffbb17e3e0dc1e6b5f823d386da1434c39b336
Rodolfo Rodrigues <rudwolf@gmail.com>
2023-01-05 14:54:59 -0300

    Fixing the app and learn about styled components

--- section-6/src/components/CourseGoals/CourseInput/CourseInput.js
+++ section-6/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,25 +1,35 @@
 import React, { useState } from 'react';
 
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import './CourseInput.scss';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = event => {
     setEnteredValue(event.target.value);
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+    setIsValid(true);
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={`form-control ${!isValid ? 'invalid' : ''} `}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
+        <div class="goalInputError">Please type something on the field above</div>
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
