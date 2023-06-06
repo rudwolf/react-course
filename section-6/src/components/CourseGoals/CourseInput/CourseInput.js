69265b35aa2a05550e22524ec7043547d23d43a0
Rodolfo Rodrigues <rudwolf@gmail.com>
2023-06-06 19:41:03 -0300

    Start of section 8

--- section-6/src/components/CourseGoals/CourseInput/CourseInput.js
+++ section-6/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,38 @@
 import React, { useState } from 'react';
 
 import Button from '../../UI/Button/Button';
-import './CourseInput.scss';
+import styles from './CourseInput.module.scss';
+
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
+
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color: ${props => (props.invalid ? 'red' : 'inherit')};
+//   }
+
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 2px solid ${props => (props.invalid ? 'red' : '#ccc')};
+//     font: inherit;
+//     background-color: ${props => (props.invalid ? '#f0b5b5' : 'transparent')};
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
+
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
+
+//   & .goalInputError {
+//     color: red;
+//     display: ${props => (props.invalid ? 'block' : 'none')};
+//   }`;
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -26,10 +57,10 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isValid ? 'invalid' : ''} `}>
+      <div invalid={!isValid} className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-        <div class="goalInputError">Please type something on the field above</div>
+        <div className={`${styles['goalInputError']} ${!isValid && styles.invalid}`}>Please type something on the field above</div>
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
--- section-6/src/components/UI/Button/Button.js
+++ section-6/src/components/UI/Button/Button.js
@@ -1,6 +1,9 @@
-import styled from 'styled-components';
+import styles from './Button.module.scss';
+
+/* import styled from 'styled-components';
 
 const Button = styled.button`
+  width: 100%;
   font: inherit;
   padding: 0.5rem 1.5rem;
   border: 1px solid #8b005d;
@@ -8,6 +11,10 @@ const Button = styled.button`
   background: #8b005d;
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
   cursor: pointer;
+
+  @media (min-width: 768px) {
+    width: auto;
+  }
 
   &:focus {
     outline: none;
@@ -19,14 +26,14 @@ const Button = styled.button`
     border-color: #ac0e77;
     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
   }
-`;
+`; */
 
-/* const Button = props => {
+const Button = props => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
-}; */
+};
 
 export default Button;
