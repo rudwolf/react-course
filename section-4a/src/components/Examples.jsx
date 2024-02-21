6b6ca733d8adabf79e538bb2dcda7b5ff20a1107
Rodolfo Rodrigues <rudwolf@gmail.com>
2024-02-21 10:05:45 -0300

    Session 4 first part ending

--- section-4a/src/components/Examples.jsx
+++ section-4a/src/components/Examples.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import TabButton from './TabButton.jsx';
 import { EXAMPLES } from '../data.js';
 import Section from './Section.jsx';
+import Tabs from './Tabs.jsx';
 
 export default function Examples() {
   const [selectedTopic, setSelectedTopic] = useState();
@@ -28,33 +29,17 @@ export default function Examples() {
 
   return (
     <Section title="Examples" id="examples">
-      <menu>
-        <TabButton
-          isSelected={selectedTopic === 'components'}
-          onClick={() => handleSelect('components')}
-        >
-          Components
-        </TabButton>
-        <TabButton
-          isSelected={selectedTopic === 'jsx'}
-          onClick={() => handleSelect('jsx')}
-        >
-          JSX
-        </TabButton>
-        <TabButton
-          isSelected={selectedTopic === 'props'}
-          onClick={() => handleSelect('props')}
-        >
-          Props
-        </TabButton>
-        <TabButton
-          isSelected={selectedTopic === 'state'}
-          onClick={() => handleSelect('state')}
-        >
-          State
-        </TabButton>
-      </menu>
-      {tabContent}
+      <Tabs buttons={
+        Object.keys(EXAMPLES).map((key) => (
+          <TabButton
+            key={key}
+            isSelected={key === selectedTopic}
+            onClick={() => handleSelect(key)}
+          >
+            {key.charAt(0).toUpperCase() + key.slice(1)}
+          </TabButton>
+        ))
+      }>{tabContent}</Tabs>
     </Section>
   );
 }
