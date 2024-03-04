59265a4092c55fa09e4def74034a3cb89fc3c182
Rodolfo Rodrigues <rudwolf@gmail.com>
2024-03-04 10:39:09 -0300

    Added name change handlers

--- section-4-tictactoe/src/components/Player/Player.jsx
+++ section-4-tictactoe/src/components/Player/Player.jsx
@@ -1,11 +1,32 @@
-export default function Player({ name, symbol }) {
+import { useState } from 'react';
+
+export default function Player({ initialName, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
+
+  function handleNameChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let playerNameElement = <span className="player-name">{playerName}</span>
+  let btnCaption = "Edit";
+
+  if (isEditing) {
+    playerNameElement = <input type="text" required value={playerName} onChange={handleNameChange} />
+    btnCaption = "Save";
+  }
+
+  function handleEditClick() {
+    setIsEditing((wasEditing) => !wasEditing);
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {playerNameElement}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{btnCaption}</button>
     </li>
   )
 }
