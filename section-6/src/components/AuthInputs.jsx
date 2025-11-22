8ce2266f073a0180a4e40ec01e0f65f2c608b31b
Rodolfo Rodrigues <rudwolf@gmail.com>
2025-11-22 19:58:40 -0300

    Finished Section 6

--- section-6/src/components/AuthInputs.jsx
+++ section-6/src/components/AuthInputs.jsx
@@ -23,8 +23,8 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && (enteredPassword.trim() === '' || enteredPassword.trim().length < 6);
 
   return (
-    <div id="auth-inputs">
-      <div className="controls">
+    <div id="auth-inputs" className='w-full max-w-sm mx-auto p-8 rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800'>
+      <div className="flex flex-col gap-2 mb-6">
           <Input label="Email" type="email" invalid={emailNotValid} onChange={(event) =>
             handleInputChange('email', event.target.value)
           } />
@@ -32,8 +32,8 @@ export default function AuthInputs() {
             handleInputChange('password', event.target.value)
           } />
       </div>
-      <div className="actions">
-        <button type="button" className="text-button">
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500 hover:cursor-pointer">
           Create a new account
         </button>
         <Button onClick={handleLogin}>Sign In</Button>
